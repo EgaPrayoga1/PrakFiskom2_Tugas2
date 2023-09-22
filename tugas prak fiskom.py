419783a9d641069447cd3b9fa809aacd8143ca74
EgaPrayoga1 <144421213+EgaPrayoga1@users.noreply.github.com>
2023-09-22 12:58:39 +0700

    Add files via upload

---
+++ tugas prak fiskom.py
@@ -0,0 +1,14 @@
+ #balok bermassa 10 Kilogram bergerak ke kanan dengan kecepatan 2m/s. Jika kecepatan diubah menjadi 4m/s dan arah tetap, maka perubahan momentum balok tersebut?
+
+m = 10  #Kg
+vi = 2   #m/s
+vf = 4   #m/s
+
+#seperti yang diketahui bahwa delta p (dp) (perubahan momentum) akan sama dengan pf-pi
+pf = 10 * 4
+print (pf, "Kgm/s")
+pi = 10 * 2
+print (pi, "Kgm/s")
+
+dp = pf - pi
+print (dp, "Kgm/s")
